ed3f760c93baa6493e64bface47e5e4dae927818
git-santanu <sddutta90@gmail.com>
2025-05-31 18:34:22 +0530

    env setup

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_NAME || 'test', process.env.DB_USERNAME || 'root', process.env.DB_PASSWORD || 'password', {
+const sequelize = new Sequelize(process.env.DB_NAME || 'my-app', process.env.DB_USERNAME || 'root', process.env.DB_PASSWORD || '', {
     host: 'localhost',
     dialect: 'mysql',
     logging: false
