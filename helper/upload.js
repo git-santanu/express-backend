d76dd287bec9fd1393a6a6e7927e5cc7bd75c37c
git-santanu <sddutta90@gmail.com>
2025-06-01 12:15:10 +0530

    multer implement

--- helper/upload.js
+++ helper/upload.js
@@ -1,14 +1,15 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require('multer')
+const path = require()
+
 const storage = multer.diskStorage({
-    destination: function(req, res, cb) {
-        cb(null, 'uploads') // where the files would be stored
+    destination: (req, res, cb) => {
+        cb(null, './uploads')
     },
-    filename: function (req, res, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) // 1717210459123
-        const ext = path.extname(file.orginalname) // returns .jpg
-        cb(null, file.fieldName + '-' + uniqueSuffix + ext) // images-1717210459123-928471829.jpg; file.fieldname → field name from form (e.g. images)
-    }
+    filename: (req, res, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random * 1E9);
+        const extensionName = path.extname(file.orginalname);
+        cb(null, file.fieldName + '-' + uniqueSuffix + extensionName)
+    } 
 })
-const upload = multer({storage: storage})
+const upload = multer({storage:storage})
 module.exports = upload
